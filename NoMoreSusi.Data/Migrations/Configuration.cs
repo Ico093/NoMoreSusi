2bc5c4d36aa643e43304532127e8c63eb7fa93df
Hristo DImitrov <hristodimitrov.work@gmail.com>
2015-06-20 00:52:10 +0300

    Revert "Seed methods"
    This reverts commit 2637abccba2d0261d794af2fee8c0be7625d33a9.

--- NoMoreSusi.Data/Migrations/Configuration.cs
+++ NoMoreSusi.Data/Migrations/Configuration.cs
@@ -1,13 +1,11 @@
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using NoMoreSusi.Models;
-
 namespace NoMoreSusi.Data.Migrations
 {
+    using System;
+    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-	using System.Linq;
+    using System.Linq;
 
-    public sealed class Configuration : DbMigrationsConfiguration<NoMoreSusiDbContext>
+    public sealed class Configuration : DbMigrationsConfiguration<NoMoreSusi.Data.NoMoreSusiDbContext>
     {
         public Configuration()
         {
@@ -15,52 +13,20 @@ namespace NoMoreSusi.Data.Migrations
             AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(NoMoreSusiDbContext context)
+        protected override void Seed(NoMoreSusi.Data.NoMoreSusiDbContext context)
         {
-	        if (!context.Roles.Any())
-	        {
-		        SeedRoles(context);
-	        }
+            //  This method will be called after migrating to the latest version.
 
-	        if (!context.Users.Any() && context.Roles.Any())
-	        {
-		        SeedUsers(context);
-	        }
-
-	        if (!context.Rooms.Any())
-	        {
-		        SeedRooms(context);
-	        }
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
         }
-
-	    private void SeedRoles(NoMoreSusiDbContext context)
-	    {
-		    var role = new IdentityRole
-		    {
-			    Name = "Administrator",
-		    };
-
-		    context.Roles.Add(role);
-	    }
-
-		private void SeedUsers(NoMoreSusiDbContext context)
-		{
-			var userStore = new UserStore<User>(context);
-			UserManager<User> um = new UserManager<User>(userStore);
-
-			var user = new User
-			{
-				UserName = "admin",
-				Email = "admin@admin.com"
-			};
-
-			var result = um.CreateAsync(user, "password").Result;
-		}
-
-	    private void SeedRooms(NoMoreSusiDbContext context)
-	    {
-	    }
-
-	    
     }
 }

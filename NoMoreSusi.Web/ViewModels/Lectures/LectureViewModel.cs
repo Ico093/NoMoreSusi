014c67a2830927878f0adda0704f694a058cac30
Hristo DImitrov <hristodimitrov.work@gmail.com>
2015-06-20 18:35:57 +0300

    Some changes to Lectures

---
+++ NoMoreSusi.Web/ViewModels/Lectures/LectureViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NoMoreSusi.Models;
+using NoMoreSusi.Web.Mapping;
+using NoMoreSusi.Web.ViewModels.Rooms;
+
+namespace NoMoreSusi.Web.ViewModels.Lectures
+{
+	public class LectureViewModel:IMapFrom<Lecture>
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public DayOfWeek Day { get; set; }
+
+		public int Hour { get; set; }
+
+		public int Length { get; set; }
+
+		public Course Course { get; set; }
+
+		public int Teacher { get; set; }
+
+		public RoomViewModel Room { get; set; }
+	}
+}
